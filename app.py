0ba9f3ce23885ec8b2e21a1a768b0d86e10fdccf
Republic Oba <republicoba1@gmail.com>
2025-10-22 14:27:36 +0100

    updated code

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from models.string_model import db
 from routes.string_route import string_db
+import os
 
 def create_app():
     app = Flask(__name__)
@@ -12,23 +13,15 @@ def create_app():
 
     app.register_blueprint(string_db)
 
-
+    with app.app_context():
+        db.create_all()
     @app.route('/')
     def home():
         return jsonify({"message": "Welcome to String-Analysis API!"})
 
-    @app.route('/health')
-    def health():
-        return jsonify({"message": "OK"}), 200
-
-    @app.before_first_request
-    def create_tables():
-        db.create_all()
-
     return app
 
 if __name__ == '__main__':
-    import os
     app = create_app()
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
